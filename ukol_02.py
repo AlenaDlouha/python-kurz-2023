b68ed812fd6a175eddb00a3108d232f9c4ad8d30
AlenaDlouha <148377200+AlenaDlouha@users.noreply.github.com>
2023-12-12 17:37:06 +0100

    Update ukol_02.py

--- ukol_02.py
+++ ukol_02.py
@@ -1,30 +1,29 @@
 sklad = {
-  "1N4148": 250,
-  "BAV21": 54,
-  "KC147": 147,
-  "2N7002": 97,
-  "BC547C": 10
+    "1N4148": 250,
+    "BAV21": 54,
+    "KC147": 147,
+    "2N7002": 97,
+    "BC547C": 10
 }
 
 print(sklad)
 
 soucastka = input("Zadej kod soucastky:")
-pocet = int(input("Zadej pocet kusu:"))
-
-print(soucastka,pocet)
-
 
 if soucastka not in sklad:
-        print("Součástka není skladem")
+    print("Součástka není skladem.")
 else:
-    pocet_soucastek_skladem = sklad[soucastka]
-    if pocet_soucastek_skladem < pocet:
-        print("Lze vydat pouze omezené množství")
-        del sklad[soucastka]
-    elif pocet_soucastek_skladem >= pocet:
-        print("Poptávku lze uspokojit v plné výši")
+    pocet = int(input("Zadej pocet kusu:"))
+
+    if pocet < 1:
+        print("Zadej platný počet kusů.")
+    elif sklad[soucastka] < pocet:
+        print("Lze vydat pouze omezené množství.")
+        print(f"Na skladě je pouze {sklad[soucastka]} kusů.")
+    else:
+        print("Poptávku lze uspokojit v plné výši.")
         sklad[soucastka] -= pocet
 
-print()
+print("Aktuální stav skladu:", sklad)
 
 
