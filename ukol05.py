6e5627a9c4f11c9a4aada5c8273008a0f4028ed3
Alena Dlouha <aadd@seznam.cz>
2023-12-12 18:24:55 +0100

    ukol 5

--- ukol05.py
+++ ukol05.py
@@ -8,15 +8,21 @@ teploty = [
     [0.4, 2.7, 1.3, -2.8]
 ]
 
-prumerne_teploty = [round(sum(den) / len(den), 2) for den in teploty]
+# Seznam průměrných teplot pro každý den
+prumerne_teploty = [sum(den) / len(den) for den in teploty]
 
+# Seznam ranních teplot
 ranni_teploty = [den[0] for den in teploty]
 
-nocni_teploty = [den[3] for den in teploty]
+# Seznam nočních teplot
+nocni_teploty = [den[-1] for den in teploty]
 
-poledni_nocni_teploty = [[den[1], den[3]] for den in teploty]
+# Seznam dvouprvkových seznamů pro polední a noční teplotu
+poledni_nocni_teploty = [[den[1], den[-1]] for den in teploty]
 
 print("Průměrné teploty pro každý den:", prumerne_teploty)
 print("Ranní teploty:", ranni_teploty)
 print("Noční teploty:", nocni_teploty)
 print("Polední a noční teploty:", poledni_nocni_teploty)
+
+
